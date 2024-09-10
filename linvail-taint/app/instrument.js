09386b55d6540dc899cfc4c6ad9652983ddc0a1a
Samuele Casarin <eleumasc@gmail.com>
2024-09-10 18:08:10 -0400

    Add runInstrument in gifc and linvail-taint

--- linvail-taint/app/instrument.js
+++ linvail-taint/app/instrument.js
@@ -1,21 +1,4 @@
 const Analysis = require("../lib/analysis");
-const { getStdin } = require("./util");
-const { promisify } = require("util");
+const { runInstrument } = require("./runInstrument");
 
-async function main() {
-  try {
-    const input = await getStdin();
-    const instrumented = Analysis.instrument(input);
-    await promisify((callback) => {
-      process.stdout.write(instrumented, callback);
-    })();
-    process.exit(0);
-  } catch (e) {
-    await promisify((callback) => {
-      process.stderr.write(String(e), callback);
-    })();
-    process.exit(1);
-  }
-}
-
-main();
+runInstrument((input) => Analysis.instrument(input));
---
+++ linvail-taint/app/runInstrument.js
@@ -0,0 +1,47 @@
+const { readFileSync, writeFileSync } = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const instrumentWithStdio = async (callback) => {
+  const input = await new Promise((resolve) => {
+    process.stdin.setEncoding("utf8");
+    let inputData = "";
+    process.stdin.on("data", function (chunk) {
+      inputData += chunk;
+    });
+    process.stdin.on("end", function () {
+      resolve(inputData);
+    });
+  });
+  const output = await callback(input);
+  await promisify((callback) => {
+    process.stdout.write(output, callback);
+  })();
+};
+
+const instrumentWithFiles = async (callback, inputPath, outputPath) => {
+  const input = readFileSync(inputPath).toString();
+  const output = await callback(input);
+  writeFileSync(outputPath, output);
+};
+
+exports.runInstrument = async (callback) => {
+  try {
+    const argv = process.argv.slice(2);
+    if (argv.length >= 2) {
+      await instrumentWithFiles(
+        callback,
+        path.resolve(argv[0]),
+        path.resolve(argv[1])
+      );
+    } else {
+      await instrumentWithStdio(callback);
+    }
+    process.exit(0);
+  } catch (e) {
+    await promisify((callback) => {
+      process.stderr.write(String(e), callback);
+    })();
+    process.exit(1);
+  }
+};
